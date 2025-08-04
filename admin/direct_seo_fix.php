91a9baf0fd5b008b9cb20ade6ea918f275be7b11
Hugo Dantas <hugodantas33@gmail.com>
2025-08-04 12:34:13 +0200

    edit tmlp copyright

--- admin/direct_seo_fix.php
+++ admin/direct_seo_fix.php
@@ -8,6 +8,10 @@
 	@author			Hugo Dantas - Agence Agerix <https://www.agerix.fr>
 	@copyright		Copyright (C) 2025. All Rights Reserved
 	@license		GNU/GPL Version 2 or later - http://www.gnu.org/licenses/gpl-2.0.html
+      __    ___  ____  __ _   ___  ____     __    ___  ____  ____  __  _  _
+	 / _\  / __)(  __)(  ( \ / __)(  __)   / _\  / __)(  __)(  _ \(  )( \/ )
+	/    \( (_ \ ) _) /    /( (__  ) _)   /    \( (_ \ ) _)  )   / )(  )  (
+	\_/\_/ \___/(____)\_)__) \___)(____)  \_/\_/ \___/(____)(__\_)(__)(_/\_)
 /------------------------------------------------------------------------------------------------------*/
 
 // Initialize Joomla framework
